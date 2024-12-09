779b08b0a1a21e487d8d7834b034967542b9213b
SanTer <67973452+SANS3R66@users.noreply.github.com>
2024-12-10 01:30:35 +0300

    implement showFloater & TID_CONNECTING_TO_SERVER
    yes baby
    i wanted to block scid button but i cant find it in v29 (skill issue mb sorry) so thats why showFloater function implemented

--- liberder.script.js
+++ liberder.script.js
@@ -24,103 +24,159 @@ const libc_send = new NativeFunction(Module.findExportByName('libc.so', 'send'),
 const libc_recv = new NativeFunction(Module.findExportByName('libc.so', 'recv'), 'int', ['int', 'pointer', 'int', 'int']);
 const htons = new NativeFunction(Module.findExportByName('libc.so', 'htons'), 'uint16', ['uint16']);
 
+const GUIInstance = base.add(0xC86B10)
+
+// edit: removed java but now retarded encode utf8 :sob:
+function encodeUtf8(str) {
+    const bytes = [];
+    for (const char of str) {
+        const codePoint = char.codePointAt(0);
+        if (codePoint <= 0x7F) {
+            bytes.push(codePoint);
+        } else if (codePoint <= 0x7FF) {
+            bytes.push(0xC0 | (codePoint >> 6));
+            bytes.push(0x80 | (codePoint & 0x3F));
+        } else if (codePoint <= 0xFFFF) {
+            bytes.push(0xE0 | (codePoint >> 12));
+            bytes.push(0x80 | ((codePoint >> 6) & 0x3F));
+            bytes.push(0x80 | (codePoint & 0x3F));
+        } else if (codePoint <= 0x10FFFF) {
+            bytes.push(0xF0 | (codePoint >> 18));
+            bytes.push(0x80 | ((codePoint >> 12) & 0x3F));
+            bytes.push(0x80 | ((codePoint >> 6) & 0x3F));
+            bytes.push(0x80 | (codePoint & 0x3F));
+        }
+    }
+    return bytes;
+}
+
+// fucking retarded method! WE are TRUE malloc ENJOYERS! no JAVA!
+const CreateNewStringObject = (Str) => {
+    const charLen = Str.length;
+    const encodedStr = encodeUtf8(Str);
+    const byteLen = encodedStr.length;
+
+    const StringObjectPtr = malloc(16);
+    StringObjectPtr.writeU32(charLen);
+    StringObjectPtr.add(4).writeU32(byteLen);
+
+    if (byteLen > 7) {
+        const LongStringAllocPtr = malloc(byteLen + 1);
+        LongStringAllocPtr.writeByteArray(encodedStr);
+        LongStringAllocPtr.add(byteLen).writeU8(0);
+        StringObjectPtr.add(8).writePointer(LongStringAllocPtr);
+    } else {
+        StringObjectPtr.add(8).writeByteArray(encodedStr);
+        StringObjectPtr.add(8 + byteLen).writeU8(0);
+    }
+
+    console.log(StringObjectPtr);
+    return StringObjectPtr;
+};
+
+function showFloater(message) {
+    message = CreateNewStringObject(message)
+    GUI_showFloaterTextAtDefaultPos(GUIInstance.readPointer(), message, 0.0, -1)
+    free(message) // memory leak lover :drooling_face:
+}
+
 let Message = {
-    _getByteStream: function(a) {
+    _getByteStream: function (a) {
         return a.add(8)
     },
-    _getVersion: function(a) {
+    _getVersion: function (a) {
         return Memory.readInt(a.add(4))
     },
-    _setVersion: function(a, b) {
+    _setVersion: function (a, b) {
         Memory.writeInt(a.add(4), b)
     },
-    _getMessageType: function(a) {
+    _getMessageType: function (a) {
         return (new NativeFunction(Memory.readPointer(Memory.readPointer(a).add(20)), 'int', ['pointer']))(a)
     },
-    _encode: function(a) {
+    _encode: function (a) {
         (new NativeFunction(Memory.readPointer(Memory.readPointer(a).add(8)), 'void', ['pointer']))(a)
     },
-    _decode: function(a) {
+    _decode: function (a) {
         (new NativeFunction(Memory.readPointer(Memory.readPointer(a).add(12)), 'void', ['pointer']))(a)
     },
-    _free: function(a) {
+    _free: function (a) {
         (new NativeFunction(Memory.readPointer(Memory.readPointer(a).add(24)), 'void', ['pointer']))(a);
         (new NativeFunction(Memory.readPointer(Memory.readPointer(a).add(4)), 'void', ['pointer']))(a)
     }
 };
 let ByteStream = {
-    _getOffset: function(a) {
+    _getOffset: function (a) {
         return Memory.readInt(a.add(16))
     },
-    _getByteArray: function(a) {
+    _getByteArray: function (a) {
         return Memory.readPointer(a.add(28))
     },
-    _setByteArray: function(a, b) {
+    _setByteArray: function (a, b) {
         Memory.writePointer(a.add(28), b)
     },
-    _getLength: function(a) {
+    _getLength: function (a) {
         return Memory.readInt(a.add(20))
     },
-    _setLength: function(a, b) {
+    _setLength: function (a, b) {
         Memory.writeInt(a.add(20), b)
     }
 };
 let Buffer = {
-    _getEncodingLength: function(a) {
+    _getEncodingLength: function (a) {
         return Memory.readU8(a.add(2)) << 16 | Memory.readU8(a.add(3)) << 8 | Memory.readU8(a.add(4))
     },
-    _setEncodingLength: function(a, b) {
+    _setEncodingLength: function (a, b) {
         Memory.writeU8(a.add(2), b >> 16 & 0xFF);
         Memory.writeU8(a.add(3), b >> 8 & 0xFF);
         Memory.writeU8(a.add(4), b & 0xFF)
     },
-    _setMessageType: function(a, b) {
+    _setMessageType: function (a, b) {
         Memory.writeU8(a.add(0), b >> 8 & 0xFF);
         Memory.writeU8(a.add(1), b & 0xFF)
     },
-    _getMessageVersion: function(a) {
+    _getMessageVersion: function (a) {
         return Memory.readU8(a.add(5)) << 8 | Memory.readU8(a.add(6))
     },
-    _setMessageVersion: function(a, b) {
+    _setMessageVersion: function (a, b) {
         Memory.writeU8(a.add(5), b >> 8 & 0xFF);
         Memory.writeU8(a.add(6), b & 0xFF)
     },
-    _getMessageType: function(a) {
+    _getMessageType: function (a) {
         return Memory.readU8(a) << 8 | Memory.readU8(a.add(1))
     }
 };
 let MessageQueue = {
-    _getCapacity: function(a) {
+    _getCapacity: function (a) {
         return Memory.readInt(a.add(4))
     },
-    _get: function(a, b) {
+    _get: function (a, b) {
         return Memory.readPointer(Memory.readPointer(a).add(POINTER_SIZE * b))
     },
-    _set: function(a, b, c) {
+    _set: function (a, b, c) {
         Memory.writePointer(Memory.readPointer(a).add(POINTER_SIZE * b), c)
     },
-    _count: function(a) {
+    _count: function (a) {
         return Memory.readInt(a.add(8))
     },
-    _decrementCount: function(a) {
+    _decrementCount: function (a) {
         Memory.writeInt(a.add(8), Memory.readInt(a.add(8)) - 1)
     },
-    _incrementCount: function(a) {
+    _incrementCount: function (a) {
         Memory.writeInt(a.add(8), Memory.readInt(a.add(8)) + 1)
     },
-    _getDequeueIndex: function(a) {
+    _getDequeueIndex: function (a) {
         return Memory.readInt(a.add(12))
     },
-    _getEnqueueIndex: function(a) {
+    _getEnqueueIndex: function (a) {
         return Memory.readInt(a.add(16))
     },
-    _setDequeueIndex: function(a, b) {
+    _setDequeueIndex: function (a, b) {
         Memory.writeInt(a.add(12), b)
     },
-    _setEnqueueIndex: function(a, b) {
+    _setEnqueueIndex: function (a, b) {
         Memory.writeInt(a.add(16), b)
     },
-    _enqueue: function(a, b) {
+    _enqueue: function (a, b) {
         pthread_mutex_lock(a.sub(4));
         let c = MessageQueue._getEnqueueIndex(a);
         MessageQueue._set(a, c, b);
@@ -128,7 +184,7 @@ let MessageQueue = {
         MessageQueue._incrementCount(a);
         pthread_mutex_unlock(a.sub(4))
     },
-    _dequeue: function(a) {
+    _dequeue: function (a) {
         let b = null;
         pthread_mutex_lock(a.sub(4));
         if (MessageQueue._count(a)) {
@@ -154,7 +210,7 @@ function OfflineBattles() {
         }
     });
     Interceptor.attach(cache.base.add(0x67FEBC), {
-        onEnter: function(a) {
+        onEnter: function (a) {
             a[3] = ptr(3)
         }
     })
@@ -171,7 +227,7 @@ function setupMessaging() {
     cache.state = cache.messaging.add(208);
     cache.loginMessagePtr = cache.messaging.add(212);
     cache.createMessageByType = new NativeFunction(cache.base.add(CREATE_MESSAGE_BY_TYPE), 'pointer', ['pointer', 'int']);
-    cache.sendMessage = function(a) {
+    cache.sendMessage = function (a) {
         Message._encode(a);
         let b = Message._getByteStream(a);
         let c = ByteStream._getOffset(b);
@@ -230,12 +286,12 @@ function setupMessaging() {
         free(e)
     }
     Interceptor.attach(Module.findExportByName('libc.so', 'pthread_cond_signal'), {
-        onEnter: function(a) {
+        onEnter: function (a) {
             onWakeup()
         }
     });
     Interceptor.attach(Module.findExportByName('libc.so', 'select'), {
-        onEnter: function(a) {
+        onEnter: function (a) {
             onReceive()
         }
     })
@@ -245,24 +301,24 @@ function ColorFull() {
     Interceptor.attach(cache.base.add(0x38C6FC), {
         onEnter(args) {
             args[7] = ptr(1);
-            }
-        });
+        }
+    });
 }
 
 function checkCpuInfo() {
-    Java.perform(function() {
+    Java.perform(function () {
         let File = Java.use("java.io.File");
         let FileInputStream = Java.use("java.io.FileInputStream");
         let InputStreamReader = Java.use("java.io.InputStreamReader");
         let BufferedReader = Java.use("java.io.BufferedReader");
-    
+
         let filePath = "/proc/cpuinfo";
-    
+
         let file = File.$new(filePath);
         let fileInputStream = FileInputStream.$new(file);
         let inputStreamReader = InputStreamReader.$new(fileInputStream);
         let bufferedReader = BufferedReader.$new(inputStreamReader);
-    
+
         let line = bufferedReader.readLine();
         while (line !== null) {
             if (line.includes("AMD") || line.includes("Intel")) {
@@ -271,7 +327,7 @@ function checkCpuInfo() {
             }
             line = bufferedReader.readLine();
         }
-    
+
         bufferedReader.close();
         inputStreamReader.close();
         fileInputStream.close();
@@ -280,7 +336,7 @@ function checkCpuInfo() {
 
 function setup(b, c) {
     Interceptor.attach(Module.findExportByName('libc.so', 'connect'), {
-        onEnter: function(a) {
+        onEnter: function (a) {
             if (ntohs(Memory.readU16(a[1].add(2))) === 9339) {
                 cache.fd = a[0].toInt32();
                 Memory.writeInt(a[1].add(4), inet_addr(Memory.allocUtf8String(b)));
@@ -294,7 +350,7 @@ function setup(b, c) {
 
 function hacksupermod() {
     const base2 = Module.findBaseAddress('lib39285EFA.so');
-    Interceptor.replace(base2.add(0x000C0D0), new NativeCallback(function(a) {
+    Interceptor.replace(base2.add(0x000C0D0), new NativeCallback(function (a) {
         return 0
     }, 'int', ['int']))
 }
@@ -302,6 +358,7 @@ function hacksupermod() {
 function init() {
     //checkCpuInfo() // uncomment to crash app if emulator is detected
     setup("127.0.0.1", 9339)
+    base.add(0xC31F2B).writeUtf8String("Erder brawl skibidi") // "TID_CONNECTING_TO_SERVER"
 }
 
 init();
